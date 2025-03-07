ea8abacefd3a521fa7ae0dfe398bfda316201fa1
Luke Chayer <luke@smellow.com>
2025-03-07 12:27:05 -0700

    Delete exercise3/src directory

--- exercise3/src/About.js
+++
@@ -1,9 +0,0 @@
-function About({title,description}) {
-  return (
-    <div>
-      <h1>{title}</h1>
-      <p>{description}</p>
-    </div>
-  );
-}
-export default About;
--- exercise3/src/Contact.js
+++
@@ -1,9 +0,0 @@
-function Contact({title, description}) {
-  return (
-    <div>
-      <h1>{title}</h1>
-      <p>{description}</p>
-    </div>
-  );
-}
-export default Contact;
