1874d6915aa8f219d8831a2cbddd84e13e4c88fe
Luke Chayer <luke@smellow.com>
2025-03-07 11:40:28 -0700

    Add files via upload

---
+++ exercise2/src/About.js
@@ -0,0 +1,8 @@
+function About({title}) {
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  );
+}
+export default About;
---
+++ exercise2/src/Contact.js
@@ -0,0 +1,8 @@
+function Contact({title}) {
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  );
+}
+export default Contact;
